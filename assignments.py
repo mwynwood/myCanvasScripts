ec723afcb2f4a8467ded0e86fe7df24cb3ce2668
Marcus <mwynwood@gmail.com>
2019-05-02 22:15:09 +1000

    Update assignments.py

--- assignments.py
+++ assignments.py
@@ -8,9 +8,21 @@ course = canvas.get_course(12345)
 
 assignments = course.get_assignments(order_by='name') # Or use bucket='ungraded' 
 
-f = open("canvas-assignments.txt", "w")
+f = open("canvas-assignments.html", "w")
+f.write("<table border='1' cellpadding='5' style='border-collapse: collapse;'>")
 for assignment in assignments:
-    if(assignment.published == True):
-        print(assignment.name)
-        f.writelines(assignment.name + "\n")
+
+    # assType = "Assignment"
+    # if(assignment.is_quiz_assignment == True):
+    #     assType="Quiz"
+
+    if(assignment.published == True and assignment.omit_from_final_grade == False):
+        f.write("<tr>")
+        f.write("<td><input type='checkbox'></td>")
+        f.write("<td><a href='" + assignment.html_url + "'>" + assignment.name + "</a></td>")
+        # f.write("<td>" + assType + "</td>")
+        # f.write("<td>" + str(assignment.assignment_group_id) + "</td>")
+        f.write("</tr>")
+
+f.write("</table>")
 f.close()
